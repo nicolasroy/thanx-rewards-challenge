c8ddc3953ea2e38c49b5f1adae75e834c707cc24
Nicolas <daitenshi.nr@gmail.com>
2025-04-10 22:33:13 -0400

    Improve the UX of the notice

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,8 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.place_order
-        format.html { redirect_back(fallback_location: root_path, notice: "Your order was successfully placed.") }
+        items = @order.line_items.map(&:itemizable).map(&:title).map(&:downcase)
+        format.html { redirect_back(fallback_location: root_path, notice: "Your #{items.to_sentence} order was successfully placed.") }
       else
         format.html { redirect_back(fallback_location: root_path, alert: "Order could not be placed: #{@order.errors.full_messages.join(', ')}") }
       end
